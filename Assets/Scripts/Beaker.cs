176b653216777b1c0547f7adb7516a0d103f51ac
teo3fl <teo3fl@gmail.com>
2021-10-12 21:47:13 +0300

    Added functionality for filling the beaker

--- Assets/Scripts/Beaker.cs
+++ Assets/Scripts/Beaker.cs
@@ -6,10 +6,25 @@ public class Beaker : MonoBehaviour
 {
     [SerializeField]
     private Transform t_contents;
+    [SerializeField]
+    private Transform t_addButton;
+    [SerializeField]
+    private GameObject go_contentSample;
 
+    // container
+
+    public void AddColor()
+    {
+        var sample = Instantiate(go_contentSample, t_contents).GetComponent<BeakerContent>();
+
+        sample.beaker = this;
+        sample.transform.SetSiblingIndex(1);
+
+        t_addButton.SetSiblingIndex(0);
+    }
 
     public void DeleteColor(GameObject color)
     {
-
+        Destroy(color);
     }
 }
