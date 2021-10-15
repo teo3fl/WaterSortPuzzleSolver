e49f94cffabd4552b9e07d768b6b1f6a6b7be45d
teo3fl <teo3fl@gmail.com>
2021-10-15 17:44:08 +0300

    Added functionality for the Exit button

---
+++ Assets/Scripts/UI/ExitManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitManager : MonoBehaviour
+{
+    public void OnExitClicked()
+    {
+        Application.Quit();
+    }
+}
